f095bd010fdc146b143d592c9c92038631c11c0f
PieroMera <U23222036@utp.edu.pe>
2026-05-28 00:23:16 -0500

    angular hecho

---
+++ semana10-master/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+export const routes: Routes = [
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  }
+];
